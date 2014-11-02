8428198edaef6be6b9a5bb1faeebad11d3fa2a74
circuitfox <circuitfox@hotmail.com>
2014-11-02 11:51:48 -0600

    Added irssi config

---
+++ .irssi/scripts/autorun/cap_sasl.pl
@@ -0,0 +1,1 @@
+/home/chris/.irssi/scripts/cap_sasl.pl
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 OLDPWD=$(pwd)
 DIR="$HOME/dotfiles"
 OLDDIR="$HOME/dotfiles.old"
-FILES=".emacs .emacs.d .i3 .Xresources .zsh_custom .zshrc .oh-my-zsh" # files and folders to symlink as dotfiles
+FILES=".emacs .emacs.d .i3 .Xresources .zsh_custom .zshrc .oh-my-zsh .irssi" # files and folders to symlink as dotfiles
 BINFILES="i3bar-conky" # files and folders to sylink to ~/bin
 
 echo "Creating backup dir: $OLDDIR"
